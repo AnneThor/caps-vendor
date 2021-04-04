c3e2773390d5d18d8ef79ed1dfbb409fd39b9e11
AnneThor <anne.thor@gmail.com>
2021-04-03 21:50:04 -0700

    added storage queue

--- vendor.js
+++ vendor.js
@@ -5,21 +5,24 @@ const faker = require('faker');
 const io = require('socket.io-client');
 
 const port = process.env.PORT || 3000;
-let store = "1-206-flowers";
-// let storeId = process.env.STORE_ID || faker.datatype.number().toString();
+let store = process.env.STORE_ID || faker.datatype.number().toString();
 
 const host = `http://localhost:${port}/caps`;
 const socket = io.connect(host);
 
 socket.emit('join', store);
 
-socket.on('delivered', thankYou)
+orderGenerator();
 
-function orderGenerator() {
+socket.emit('getAll', {client: store, event: 'delivered' });
+
+
+socket.on('delivered', thankYou);
+
+function orderGenerator () {
   setInterval( () => {
     let order = {
       store: store,
-      // storeId: storeId,
       orderID: faker.datatype.uuid(),
       customer: faker.name.findName(),
       address: faker.address.streetAddress(),
@@ -28,11 +31,10 @@ function orderGenerator() {
   }, 5000)
 }
 
-function thankYou(payload) {
+function thankYou (payload) {
   console.log(`Thank you for delivering ${payload.orderID}!`);
+  socket.emit('received', store);
 }
-
-orderGenerator();
 
 module.exports = {
   makeOrders: orderGenerator,
